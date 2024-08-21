0add04f2e8f71e0f775524e87eadc716a7c12d45
尤雨东 <root@ltpp.vip>
2024-08-21 13:47:50 +0800

    feat:update

--- sh/build.sh
+++ sh/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RUSTFLAGS="-Ctarget-feature=+crt-static" cargo build --release;
+docker run --rm -v "$(pwd):/tmp/test-code" ccr.ccs.tencentyun.com/linux_environment/cargo:1.0.0 /bin/bash -c "source ~/.bashrc;cd /tmp/test-code && cargo build --release --target=x86_64-unknown-linux-musl";
 echo "Press Enter to continue...";
 read -n 1;
