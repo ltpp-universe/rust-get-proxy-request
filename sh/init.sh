05005ed7c5110b28400b26a8b04d1d9f85543362
尤雨东 <root@ltpp.vip>
2025-02-07 23:10:23 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/rust-get-proxy-request.git;
-git remote add github git@github.com:ltpp-universe/rust-get-proxy-request.git;
+git init;
+git config core.ignorecase false;
+git remote add gitee git@gitee.com:ltpp-universe/rust-get-proxy-request.git;
 git remote add origin git@github.com:ltpp-universe/rust-get-proxy-request.git;
