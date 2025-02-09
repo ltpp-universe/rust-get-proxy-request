7c9d98af649f9af6766bdb7172c620adbdc592fd
尤雨东 <root@ltpp.vip>
2025-02-09 22:04:09 +0800

    n

--- sh/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init;
-git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/rust-get-proxy-request.git;
-git remote add origin git@github.com:ltpp-universe/rust-get-proxy-request.git;
